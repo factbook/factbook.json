b3c41c782b0e2708512047c3d7204b99d10984dd
Martin Trobäck <martintk@axis.com>
2016-11-07 14:28:47 +0100

    Alternate index and code comments

--- buildIndex.js
+++ buildIndex.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 
-const exclude = ['almanac.md', 'readme.md', 'summary.md', 'buildindex.js', 'package.json', 'meta', '.git', 'countryindex.json'];
+const exclude = ['almanac.md', 'readme.md', 'summary.md', 'buildindex.js',
+'package.json', 'meta', '.git', 'countryindex.json', 'alternatecountryindex.json'];
 
 const countryIndex = [];
+const alternateIndex = {};
 
+// Read files in sub directories and extract country information
 const readFile = (dir, file) => {
   const country = JSON.parse(fs.readFileSync(`${dir}/${file}`).toString());
   const currentCountry = {
@@ -16,8 +19,18 @@ const readFile = (dir, file) => {
     country.Government['Country name']['conventional short form'].text : '';
 
   countryIndex.push(currentCountry);
+
+  if (!alternateIndex[dir]) {
+    alternateIndex[dir] = [];
+  }
+
+  alternateIndex[dir].push({
+    filename: file,
+    country: currentCountry.country
+  });
 };
 
+// Find files in sub directories
 const readDir = (dir) => {
   if (!exclude.includes(dir.toLowerCase())) {
     const continent = fs.readdirSync(dir);
@@ -27,9 +40,12 @@ const readDir = (dir) => {
   }
 };
 
+// Read the directories in the root folder
 const continents = fs.readdirSync('./');
 continents.forEach((continent) => {
   readDir(continent);
 });
 
+// Create output files
 fs.writeFileSync('countryIndex.json', JSON.stringify(countryIndex, null, 2));
+fs.writeFileSync('alternateCountryIndex.json', JSON.stringify([alternateIndex], null, 2));
